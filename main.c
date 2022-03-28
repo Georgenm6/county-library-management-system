30af15094e79f45ff76fd212a8f3fcf10f820830
Georgenm6 <56872036+Georgenm6@users.noreply.github.com>
2022-03-28 08:28:58 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,118 +1,146 @@
-/*
-    County Library System,
-    by, George mbugua,
-    Feb 2022,
-    MIT License,
-    C99 Compiler,
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Patron
-{
-    char name[100];
+#define FILE_NAME  "Libpatron.bin"
+
+struct Patron{
+    int id;
+    char name[50];
     char email[50];
-    char password[30];
-    int is_staff;
+    char password[50];
 };
-struct Book
-{
-    char name[100];
+
+struct Book{
+    int id;
     char title[50];
-    char ISBN[30];
+    char author[50];
+    char summary[50];
 };
 
 void menu();
 void add_patron();
+void view_patron();
 void add_book();
+void view_book();
+
+int main(){
+
+ char title [ 50 ] = "county library management";
+ char name  [ 10 ] = "GEORGE";
+ printf( "\t\t%s\n\t\t%s\n", title, name );
+ menu();
 
 
-int main()
-{
-    char title[100] = "County Library Management";
-    char status[100] = "welcome George";
+
+}
+
+void menu() {
     int action;
-
-    printf("\n\n\n\t\t\t   ****************\n\t\t\t%s\n \t\t\t\    %s\n\t\t\t   ****************\n", title, status);
-    menu();
-	return 0;
-}
-
-void menu()
- {
-    int action = 0;
-
     do {
-        printf("Select an action below\n");
-        printf("1. Add new Patron\n");
-        printf("2. view all patrons\n");
-        printf("3. Add new book\n");
-        printf("4. View all books\n");
-        printf("0. Exit program\n");
-        printf("\nYour actions: ");
-        scanf("%d", &action);
-        if(action < 1 || action > 4)
-        {
-            printf("invalid action\n");
+        printf ("\t\t 1. Add patron\n\t\t 2. View patron\n\t\t 3. Add book\n\t\t 4. View book\n\t\t 0. Exit\n \t\t Select one action: ") ;
+        scanf("%d", &action );
+
+        switch (action){
+        case 1:
+             add_patron();
+             break;
+        case 2:
+            view_patron();
+            break;
+        case 3:
+             add_book();
+             break;
+        case 4:
+             view_book();
+             break;
+        case 0:
+            exit(1);
+            break;
+        default:
+            printf("invalid action!!!");
         }
 
-        switch(action) {
-            case 1:
-                add_patron();
-                break;
-            case 2:
-                printf(" view all patrons\n");
-                break;
-            case 3:
-                add_book();
-                break;
-            case 4:
-                printf(" View all resources\n");
-                break;
-            case 0:
-                exit(1);
-                break;
-            default:
-                printf("Wrong Entry.");
-
-        }
+        system("cls");
     } while (action != 0);
+
+
+
 }
 
-void add_patron()
-{
+void add_patron(){
     system("cls");
+    FILE *fp;
+    fp = fopen(FILE_NAME,"a+b");
+
     struct Patron patron;
-    printf("Create Patron");
-    printf("enter Name: ");
-    getchar();
-    gets(patron.name);
-    printf("enter email: ");
-    gets(patron.email);
-    printf("enter initial password: ");
-    gets(patron.password);
-    printf("enter 1 if staff 0 otherwise: ");
-    scanf("%d", &patron.is_staff);
+    printf("\t\t\t Add new Patron.\n\n");
+    printf ("\t\t enter name: ");
+    scanf("%s", patron.name);
+    printf ("\t\t enter email: ");
+    scanf("%s", patron.email);
+    printf ("\t\t enter password: ");
+    scanf("%s", patron.password);
 
-    printf("Enter any key to go to main....\n");
+    fwrite(&patron,sizeof(struct Patron),1,fp);
+    fclose(fp);
+
+    printf("\n\n\n\t\t\tPress any key to go to main menu.....");
     getchar();
-    system("cls");
 }
 
-void add_book()
-{
+ void view_patron(){
     system("cls");
+    FILE *fp;
+    struct Patron patron;
+    fp = fopen(FILE_NAME,"r+b");
+    fread(&patron,sizeof(struct Patron),1,fp);
+    getchar();
+    printf ("\t\t Patron %s.\n", patron.name);
+    printf ("\t\t Email %s.\n", patron.email);
+    printf ("\t\t Password %s.\n", patron.password);
+    getchar();
+    fclose(fp);
+
+    printf("\n\n\n\t\t\tPress any key to go to main menu.....");
+    getchar();
+ }
+
+ void add_book(){
+    system("cls");
+    FILE *fp;
+    fp = fopen(FILE_NAME,"a+b");
+
     struct Book book;
-    printf("Enter book: ");
-    getchar();
-    gets(book.name);
-    printf("Enter title: ");
-    gets(book.title);
-    printf("Enter ISBN: ");
-    gets(book.ISBN);
+    printf("\t\t\t Add new Book.\n\n");
+    printf ("\t\t Enter Book Title: ");
+    scanf("%s", book.title);
+    printf ("\t\t enter the book author: ");
+    scanf("%s", book.author);
+    printf ("\t\t enter a synopsis: ");
+    scanf("%s", book.summary);
 
-    printf("Enter any key to go to main....\n");
+    fwrite(&book,sizeof(struct Book),1,fp);
+    fclose(fp);
+
+    printf("\n\n\n\t\t\tPress any key to go to main menu.....");
     getchar();
+ }
+
+ void view_book(){
     system("cls");
-}
+    FILE *fp;
+    struct Book book;
+    fp = fopen(FILE_NAME,"r+b");
+    fread(&book,sizeof(struct Book),1,fp);
+    getchar();
+    printf (" \t\t\t\t All Books.\n");
+    printf ("\t\t Title: %s.\n", book.title);
+    printf ("\t\t Author %s.\n", book.author);
+    printf ("\t\t Summary %s.\n", book.summary);
+    getchar();
+    fclose(fp);
+
+    printf("\n\n\n\t\t\tPress any key to go to main menu.....");
+    getchar();
+ }
+
